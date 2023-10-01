0e06ba57722df5beb76de053360fee13e14ed9c7
dani7514 <danielmisganaw192021@gmail.com>
2023-10-02 01:04:25 +0300

    update multer

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule,{cors: true});
-  app.useStaticAssets(join(__dirname, '../images'))
+  // app.useStaticAssets(join(__dirname, '../images'))
   await app.listen(3000);
 }
 bootstrap();
--- src/music/music.controller.ts
+++ src/music/music.controller.ts
@@ -42,39 +42,39 @@ export class MusicController {
         return await this.musicService.deleteMusic(id);
     }
 
-    @Post('/upload/:id')
-    @UseInterceptors(FileInterceptor('image',{
-        storage: diskStorage({
-            destination: "./images/upload",
-            filename: (req, file, cb) => {
-                const name = file.originalname.split('.')[0];
-                const fileExtention = file.originalname.split('.')[1];
-                const newFilename = name + '_' + Date.now() + '.' + fileExtention;
+    // @Post('/upload/:id')
+    // @UseInterceptors(FileInterceptor('image',{
+    //     storage: diskStorage({
+    //         destination: "./images/upload",
+    //         filename: (req, file, cb) => {
+    //             const name = file.originalname.split('.')[0];
+    //             const fileExtention = file.originalname.split('.')[1];
+    //             const newFilename = name + '_' + Date.now() + '.' + fileExtention;
 
-                cb(null, newFilename);
-                },
-        }),
-        fileFilter: (req, file, cb) => {
-            if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-              return cb(null, false);
-            }
-            return cb(null, true);
-          },
-    })
-    )
-    async uploadFile(
-        @UploadedFile() file: Express.Multer.File ,
-        @Param('id') id: string,
-    ){
-        if (!file) {
-            throw new BadRequestException('File is not an image');
-          } else {
-            const response = { image: `upload/${file.filename}` };
-            return await this.musicService.updateMusic(id,{
-                image: response.image
-            })
-          } 
-    }
+    //             cb(null, newFilename);
+    //             },
+    //     }),
+    //     fileFilter: (req, file, cb) => {
+    //         if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    //           return cb(null, false);
+    //         }
+    //         return cb(null, true);
+    //       },
+    // })
+    // )
+    // async uploadFile(
+    //     @UploadedFile() file: Express.Multer.File ,
+    //     @Param('id') id: string,
+    // ){
+    //     if (!file) {
+    //         throw new BadRequestException('File is not an image');
+    //       } else {
+    //         const response = { image: `upload/${file.filename}` };
+    //         return await this.musicService.updateMusic(id,{
+    //             image: response.image
+    //         })
+    //       } 
+    // }
 
 
     

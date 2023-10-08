1e90b5a038552caac94a9fa51080f151163184c5
dani7514 <danielmisganaw192021@gmail.com>
2023-10-08 14:53:54 +0300

    fit cors

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule,{cors: true});
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // app.useStaticAssets(join(__dirname, '../images'))
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: 'Content-Type',
+  };
+
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
